b0a4d467025f0ce94e9b23f743d4c13885d31bc3
akshayathangallapally <ta7440360@gmail.com>
2025-02-27 09:15:41 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,17 @@
+#concatenation
+t1=(1,2,3)
+t2=(4,5,6)
+print(t1+t2)
+
+#repetation
+a=(1,2,3,3,42,23)
+print(a*2)
+
+#iteration
+c=(1,2,3,3,42,23)
+for i in c:
+    print(i)
+
+#membership
+c=(1,2,3,3,42,23)
+print(1 in c)
